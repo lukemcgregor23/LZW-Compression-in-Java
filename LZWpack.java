1f03c5e70fcee6d703ac0660e33091dd22175750
KieraL <62452203+KieraL@users.noreply.github.com>
2020-03-21 13:27:40 +1300

    Create LZWpack.java

---
+++ LZWpack.java
@@ -0,0 +1,39 @@
+import java.io.Console;
+
+class LZWpack {
+	
+	public static void main(String[] args) {
+		
+		int topPhraseNumber = 15;
+		long buffer = 0L;
+		int bufferLength = 0;
+		
+		while (System.console().hasNextLine()) {
+			int phraseNumber = Integer.parseInt(System.console().readLine());
+			int phraseLength = calcBitLength(topPhraseNumber);
+			
+			buffer |= phraseNumber << bufferLength;
+			bufferLength += phraseLength;
+			
+			while (bufferLength >= 8)
+			{
+				System.console().write(buffer & ((1 << 8) - 1));
+				buffer = buffer >>> 8;
+				bufferLength -= 8;
+			}
+		}
+		
+		if (bufferLength > 0)
+			System.console().write(buffer);
+	}
+	
+	private static int calcBitLength(int n)
+	{
+		int l = 1;
+		while (n > 1)
+		{
+			n = n >>> 1;
+			l++;
+		}
+	}
+}
