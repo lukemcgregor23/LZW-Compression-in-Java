6eab49e955fc1a613158b072ce65e753b68e2a28
McGregor, Luke Duff <lukemcgregs@gmail.com>
2020-03-21 14:31:32 +1300

    just working on shit

---
+++ LZWencode.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class LZWencode {
+
+    public static void main(String[] args) {
+        File file = new File("inputfile.txt");
+        FileInputStream fin = null;
+        try {
+            // create FileInputStream object
+            fin = new FileInputStream(file);
+
+            byte fileContent[] = new byte[(int)file.length()];
+
+            // Reads up to certain bytes of data from this input stream into an array of bytes.
+            fin.read(fileContent);
+            //create string from byte array
+            String s = new String(fileContent);
+            System.out.println("File content: " + s);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found" + e);
+        }
+        catch (IOException ioe) {
+            System.out.println("Exception while reading file " + ioe);
+        }
+        finally {
+            // close the streams using close method
+            try {
+                if (fin != null) {
+                    fin.close();
+                }
+            }
+            catch (IOException ioe) {
+                System.out.println("Error while closing stream: " + ioe);
+            }
+        }
+    }
+
+}
+
+class dictionary {
+    public static void dictionary(){
+        LinkedList<Integer> list = new LinkedList<Integer>();
+    }
+    public int get(p) {
+        list.indexOf(p)
+    }
+    public int add(p){
+        list.add(p)
+    }
+
+}
